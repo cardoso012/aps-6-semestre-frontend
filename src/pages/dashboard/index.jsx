9c8135740bb205b0a4440d6463087951a3481834
cardoso012 <lucas.cardoso1228@gmail.com>
2024-11-20 15:16:15 -0300

    feat: adicionando detalhes à página de dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,3 +1,15 @@
 export function DashboardPage() {
-    return <h1>Dashboard</h1>;
+    return (
+        <div className="bg-stone-50 flex justify-center items-center h-screen">
+
+            <div className="h-96 rounded-md p-5 flex flex-col items-center">
+                <span 
+                    className="border border-blue-600 shadow-md py-2 px-3 rounded-md font-semibold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-3xl inline-block text-transparent bg-clip-text"
+                >
+                    Olá, bem vindo ao Dashboard!
+                </span>
+            </div>
+
+        </div>
+    );
 }
